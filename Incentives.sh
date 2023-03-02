1644e8087e7a6c1e72b2ece7dd0e973f4aa330ac
Hieu Luong <39450217+luonghieu184@users.noreply.github.com>
2023-03-02 15:43:28 +0700

    Update Incentives.sh

--- Incentives.sh
+++ Incentives.sh
@@ -5,15 +5,11 @@ source <(curl -s https://raw.githubusercontent.com/luonghieu184/Nibiru-Cronjob/m
 echo -e '\n\e[42mRunning cronj\e[0m\n' && sleep 1
 
 # Faucet NIBIRU token
-FAUCET_URL="https://faucet.itn-1.nibiru.fi/"
-DWALLET=$(nibid keys show wallet -a)
-curl -X POST -d '{"address": "'"$DWALLET"'", "coins": ["11000000unibi","100000000unusd","100000000uusdt"]}' https://faucet.itn-1.nibiru.fi/
+curl -X POST -d '{"address": "'"$(nibid keys show wallet -a)"'", "coins": ["11000000unibi","100000000unusd","100000000uusdt"]}' https://faucet.itn-1.nibiru.fi/
 
 # Delegates
 # $(nibid keys show wallet --bech val -a) 
-#DVALIDATOR = Another validator address
 
-DVALIDATOR=$(nibid keys show wallet --bech val -a) 
-nibid tx staking delegate $DVALIDATOR 1000000unibi --from wallet --chain-id nibiru-itn-1 --fees 5000unibi --yes
+nibid tx staking delegate $(nibid keys show wallet --bech val -a) 1000000unibi --from wallet --chain-id nibiru-itn-1 --fees 5000unibi --yes
 
 echo -e "- Success Running. HGL welcome! \e[0m"
